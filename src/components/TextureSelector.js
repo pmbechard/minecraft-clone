7b6bf178fc3caf28645f561868b91c916debf0e0
Peyton Bechard <pbechard@my.yorku.ca>
2022-11-27 18:19:35 +0800

    Adds visual component to texture selector

--- src/components/TextureSelector.js
+++ src/components/TextureSelector.js
@@ -1,6 +1,15 @@
 import { useEffect, useState } from 'react';
 import useKeyboard from '../hooks/useKeyboard';
 import { useStore } from '../hooks/useStore';
+import { dirtImg, grassImg, glassImg, woodImg, logImg } from '../images/images';
+
+const images = {
+  dirt: dirtImg,
+  grass: grassImg,
+  glass: glassImg,
+  wood: woodImg,
+  log: logImg,
+};
 
 const TextureSelector = () => {
   const [visible, setVisible] = useState(false);
@@ -14,7 +23,7 @@ const TextureSelector = () => {
   useEffect(() => {
     const pressedTexture = Object.entries(textures).find(([k, v]) => v);
     if (pressedTexture) setTexture(pressedTexture[0]);
-    pressedTexture && console.log('pressed: ' + pressedTexture[0]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [setTexture, dirt, grass, glass, wood, log]);
 
   useEffect(() => {
@@ -25,7 +34,22 @@ const TextureSelector = () => {
     return () => clearTimeout(visibilityTimeout);
   }, [activeTexture]);
 
-  return visible && <div className='absolute centered'>TextureSelector</div>;
+  return (
+    visible && (
+      <div className='absolute centered texture-selector'>
+        {Object.entries(images).map(([k, src]) => {
+          return (
+            <img
+              src={src}
+              key={k}
+              alt={k}
+              className={`${k === activeTexture ? 'active' : ''}`}
+            />
+          );
+        })}
+      </div>
+    )
+  );
 };
 
 export default TextureSelector;
