d412b3a48beb3df4a3a5be103eed6e58adb8b5c8
Peyton Bechard <pbechard@my.yorku.ca>
2022-11-27 16:24:44 +0800

    Adds ground plane and player cam (without movement)

--- src/images/images.js
+++ src/images/images.js
@@ -1,15 +1,7 @@
-import dirtImg from './dirt.jpg';
-import grassImg from './grass.jpg';
+import dirtImg from './dirt.jpeg';
+import grassImg from './grass.jpeg';
 import glassImg from './glass.png';
-import logImg from './log.jpg';
+import logImg from './log.jpeg';
 import woodImg from './wood.png';
 
-const images = {
-  dirtImg,
-  grassImg,
-  glassImg,
-  woodImg,
-  logImg,
-};
-
-export default images;
+export { dirtImg, grassImg, glassImg, woodImg, logImg };
