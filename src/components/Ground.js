d81e0c64bb58462453f4270f6d440fec8e28441d
Peyton Bechard <pbechard@my.yorku.ca>
2022-11-27 17:48:37 +0800

    Adds ability to add/remove cubes

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,21 +1,25 @@
 import { usePlane } from '@react-three/cannon';
-import { NearestFilter } from 'three';
-import { RepeatWrapping } from 'three';
 import { groundTexture } from '../images/textures';
+import { useStore } from '../hooks/useStore';
 
 export const Ground = () => {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
-    position: [0, 0, 0],
+    position: [0, -0.5, 0],
   }));
+  const [addCube] = useStore((state) => [state.addCube]);
 
-  groundTexture.magFilter = NearestFilter;
-  groundTexture.wrapS = RepeatWrapping;
-  groundTexture.wrapT = RepeatWrapping;
   groundTexture.repeat.set(100, 100);
 
   return (
-    <mesh ref={ref}>
+    <mesh
+      ref={ref}
+      onClick={(e) => {
+        e.stopPropagation();
+        const [x, y, z] = Object.values(e.point).map((val) => Math.ceil(val));
+        addCube(x, y, z);
+      }}
+    >
       <planeBufferGeometry attach='geometry' args={[100, 100]} />
       <meshStandardMaterial attach='material' map={groundTexture} />
     </mesh>
